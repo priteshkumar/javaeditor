9305d5e6c1a8bb2b52db7bf8693b39e6be21fbdd
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-02-14 13:15:08 +0530

    add production changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn javaide.wsgi --log-file -
--- javaide/settings.py
+++ javaide/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r@hz1g$v&)kp1uhrdvxe(x5^_g8p71!nym9a*zq*y&7tt3fkv-'
-
+#SECRET_KEY = 'r@hz1g$v&)kp1uhrdvxe(x5^_g8p71!nym9a*zq*y&7tt3fkv-'
+SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
